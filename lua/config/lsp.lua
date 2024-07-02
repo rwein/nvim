97ffbdb70391d6da333475a0633f88dab7fc661b
Rob Weinhandl <robweinhandl@gmail.com>
2024-07-02 14:58:21 -0700

    Move all lsp config into dedicated file

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,14 @@
--- Set up LSPs
+-- Set up LSPs & Autocomplete
 local lspconfig = require('lspconfig')
+local cmp = require('cmp')
+local lsp_zero = require('lsp-zero')
 
-lspconfig.phpactor.setup({}) 
+-- Set default keymaps when an LSP attaches to a buffer
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+lspconfig.phpactor.setup({})
+lspconfig.lua_ls.setup({})
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -4,15 +4,5 @@ return {
     {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'}, {'L3MON4D3/LuaSnip'},
-    config = function()
-        local lsp_zero = require('lsp-zero')
- 
-        -- Set default keymaps when an LSP attaches to a buffer
-        lsp_zero.on_attach(function(client, bufnr)
-          -- see :help lsp-zero-keybindings
-          -- to learn the available actions
-          lsp_zero.default_keymaps({buffer = bufnr})
-        end)
-    end
+    {'hrsh7th/nvim-cmp'}, {'L3MON4D3/LuaSnip'}
 }
