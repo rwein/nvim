d95e5b0a6e31a60278f72bded458811be66288cc
Rob Weinhandl <robweinhandl@gmail.com>
2024-07-02 17:12:16 -0700

    Configure Intelephense
    Works better than PHPActor

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -10,5 +10,8 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-lspconfig.phpactor.setup({})
+-- PHPActor is an opensource LSP for PHP, but it didn't work as well as Intelephense for me (as of Jul 2024). Keeping
+-- It here in case I want to try it again in the future.
+-- lspconfig.phpactor.setup({})
+lspconfig.intelephense.setup({}) -- Closed source and some features pay walled but works well for php
 lspconfig.lua_ls.setup({})
