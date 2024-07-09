3e21239e264b908af8c2275baa38fa2953264436
Rob Weinhandl <robweinhandl@gmail.com>
2024-07-09 10:53:01 -0700

    Never use LSP for auto formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,7 +9,7 @@ return {
             format_on_save = {
                 -- These options will be passed to conform.format()
                 timeout_ms = 500,
-                lsp_format = "fallback",
+                lsp_format = "never",
             },
             formatters_by_ft = {
                 lua = { "stylua" },
