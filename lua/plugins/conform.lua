0270b570d9faaa0d6d6c399c06c054763f7bb7ca
Rob Weinhandl <robweinhandl@gmail.com>
2024-07-09 10:41:19 -0700

    Add conform plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+-- Automatically format code on save
+-- https://github.com/stevearc/conform.nvim
+
+return {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                json = { "prettier" },
+                -- Use a sub-list to run only the first available formatter
+                php = { { "pint", "php-cs-fixer" } },
+            },
+        })
+    end,
+}
