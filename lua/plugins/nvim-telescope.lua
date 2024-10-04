0e3097d4bfc141e9aea8909f452cf8ac779e3f10
Rob Weinhandl <robweinhandl@gmail.com>
2024-10-04 16:15:04 -0700

    Update telescope bindings

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -8,11 +8,13 @@ return {
         -- Customize telescope shortcuts. Note this are copied over in nvim-tree.lua so they also work when we're
         -- in file explorer.
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Search for files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search with grep" })
         vim.keymap.set("n", "<leader>fb", function()
             builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
-        end, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        end, { desc = "Search for buffers" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search for help tags" })
+        vim.keymap.set("n", "<leader>fo", builtin.lsp_dynamic_workspace_symbols, { desc = "Search workspace symbols" })
+        vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Search current document symbols" })
     end,
 }
